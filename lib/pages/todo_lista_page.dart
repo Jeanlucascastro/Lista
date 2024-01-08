96f57457f4cf25a8344df5d789c28c7030bfbd87
Jean Castro <61846534+Jeanlucascastro@users.noreply.github.com>
2024-01-07 22:25:10 -0300

    Todo list working

--- lib/pages/todo_lista_page.dart
+++ lib/pages/todo_lista_page.dart
@@ -1,8 +1,17 @@
 import 'package:flutter/material.dart';
 
 
-class TodoListPage extends StatelessWidget {
-  const TodoListPage({super.key});
+class TodoListPage extends StatefulWidget {
+  TodoListPage({super.key});
+
+  @override
+  State<TodoListPage> createState() => _TodoListPageState();
+}
+
+class _TodoListPageState extends State<TodoListPage> {
+  List<String> todos = [];
+
+  final TextEditingController todoController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -17,6 +26,7 @@ class TodoListPage extends StatelessWidget {
               children: [
                 Expanded(
                   child: TextField(
+                    controller: todoController,
                     decoration: InputDecoration(
                         border: OutlineInputBorder(),
                         labelText: 'Adicione uma tarefa'
@@ -26,10 +36,16 @@ class TodoListPage extends StatelessWidget {
 
                 SizedBox(width: 8),
                 ElevatedButton(
-                    onPressed: () {},
+                    onPressed: () {
+                      String text = todoController.text;
+                      setState(() {
+                        todos.add(text);
+                      });
+                      todoController.clear();
+                    },
                     style: ElevatedButton.styleFrom(
                     backgroundColor: Color(0xff00d7f3),
-                    padding: EdgeInsets.all(16) 
+                    padding: EdgeInsets.all(16)
                   ),
                     child: Icon(
                       Icons.add,
@@ -42,7 +58,15 @@ class TodoListPage extends StatelessWidget {
             ListView(
               shrinkWrap: true,
               children: [
-                
+                for(String todo in todos)
+                  ListTile(
+                    title: Text(todo),
+                    subtitle: Text('ola'),
+                    leading: Icon(Icons.save, size: 30,),
+                    onTap: () {
+                      print('tarefa: $todo');
+                    },
+                  ),
               ],
             ),
             SizedBox(height: 16,),
